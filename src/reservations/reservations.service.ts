b79122c20aa35cc025f10a55b6f5c34d48205101
AKSEL01A <123596469+AKSEL01A@users.noreply.github.com>
2025-06-05 14:59:29 +0200

    Update reservations.service.ts

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -379,6 +379,18 @@ export class ReservationsService {
       .getMany();
   }
 
+async getReservationsByRestaurant(restaurantId: string): Promise<ReservationTable[]> {
+    return this.reservationRepository
+      .createQueryBuilder('reservation')
+      .leftJoinAndSelect('reservation.table', 'table')
+      .leftJoinAndSelect('table.restaurantBloc', 'bloc')
+      .leftJoinAndSelect('bloc.restaurant', 'restaurant')
+      .leftJoinAndSelect('reservation.reservationTime', 'reservationTime')
+      .leftJoinAndSelect('reservation.user', 'user')
+      .where('restaurant.id = :restaurantId', { restaurantId })
+      .orderBy('reservationTime.date2', 'DESC')
+      .getMany();
+  }
 
   async getReservationById(id: string) {
     const fetchedReservation = await this.reservationRepository.findOneBy({ id: id });
